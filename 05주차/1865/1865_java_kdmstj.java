39c89f50cc77372e1271f03f1edfe7e4fa558b4c
kdmstj <rkddmstj429@gmail.com>
2025-10-22 15:27:42 +0900

    add: 5주차

---
+++ 05주차/1865/1865_java_kdmstj.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+
+public class ans1865 {
+    static int TC, N, M, W;
+    static List<Edge> edges;
+    static int[] dist;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        TC = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int testCase = 0; testCase < TC; testCase++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
+
+            edges = new ArrayList<>();
+            dist = new int[N + 1];
+            Arrays.fill(dist, 0); //모든 정점에 0 비용
+
+            // 도로 : 양방향 + T
+            for (int i = 1; i <= M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int S = Integer.parseInt(st.nextToken());
+                int E = Integer.parseInt(st.nextToken());
+                int T = Integer.parseInt(st.nextToken());
+                edges.add(new Edge(S, E, T));
+                edges.add(new Edge(E, S, T));
+            }
+
+            // 웜홀 : 단방향 - T
+            for (int i = 1; i <= W; i++) {
+                st = new StringTokenizer(br.readLine());
+                int S = Integer.parseInt(st.nextToken());
+                int E = Integer.parseInt(st.nextToken());
+                int T = Integer.parseInt(st.nextToken());
+                edges.add(new Edge(S, E, -T));
+            }
+
+            boolean isCycle = bellmanFord();
+            sb.append(isCycle ? "YES" : "NO").append('\n');
+        }
+        System.out.println(sb.toString());
+    }
+
+    private static boolean bellmanFord() {
+        for (int i = 1; i <= N; i++) {
+            boolean updated = false;
+            for (Edge edge : edges) {
+                if (dist[edge.from] + edge.cost < dist[edge.to]) {
+                    dist[edge.to] = dist[edge.from] + edge.cost;
+                    updated = true;
+                }
+            }
+            if (!updated) break;
+        }
+
+        for (Edge e : edges) {
+            if (dist[e.to] > dist[e.from] + e.cost) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static class Edge {
+        int from;
+        int to;
+        int cost;
+
+        Edge(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
+    }
+}
