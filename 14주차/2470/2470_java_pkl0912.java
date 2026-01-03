faa90a94dd999bbfc1f320dc22a5213eaa2dbc69
pkl0912 <pkl0912@naver.com>
2026-01-03 19:11:47 +0900

    feat:2470

--- 14주차/2470/2470_java_pkl0912.java
+++ 14주차/2470/2470_java_pkl0912.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class 2470_java_Kyounglin{
+public class Main{
 
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
